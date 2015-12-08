9ad01e7a70426be3435d290c915d9fd7928cc3c8
Sean Graham <github@sean-graham.com>
2015-12-07 20:37:35 -0500

    commented

--- youtube-deembedder.js
+++ youtube-deembedder.js
@@ -1,3 +1,10 @@
+
+/*
+ * youtube-deembedder.js
+ *
+ * Finds all of the embedded youtube videos in the current page and opens
+ * them in new tabs.
+ */
 javascript:(function() {
     $("iframe[src^='https://www.youtube']").each(function(a) {
         var link = $(this).attr("src");
