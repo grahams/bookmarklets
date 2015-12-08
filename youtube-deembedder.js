d91c5405e06a7dde3d6e401ecf14c73624199c30
Sean Graham <github@sean-graham.com>
2015-12-08 14:06:38 -0500

    cleaned up the code a bit and handle the situation where the browser is currently vieweing an embed url directly.

--- youtube-deembedder.js
+++ youtube-deembedder.js
@@ -6,21 +6,42 @@
  * them in new tabs.
  */
 javascript:(function() {
-    // Find all of the embedded videos in the document
-    var links = document.querySelectorAll("iframe[src^='https://www.youtube']");
-        
-    for(var x = 0; x < links.length; x += 1) {
-        if(links) {
-            // Get the embed URL
-            link = links[x].src;
+    var embedToWatch = function(url) {
+        if(url) {
+            // convert the embed URL into a watch URL
+            link = url.replace(/^.*embed\/(.*)/gm, 
+                    "https://www.youtube.com/watch?v=$1");
 
-            if(link) {
-                // convert the embed URL into a watch URL
-                link = link.replace(/^.*embed\/(.*)/gm, 
-                        "https://www.youtube.com/watch?v=$1");
+            return link;
+        }
+    };
 
-                // open the URL in a new tab/window
-                window.open(link);
+    var url;
+    var loc = window.location.href;
+
+    if(loc && loc.match(/^http(s)?:\/\/(www.)?youtube.com\/embed\/.*/m)) {
+        // we are directly on a youtube embed page, so just pop it out
+        url = embedToWatch(loc);
+
+        window.open(url);
+    }
+    else {
+        // Find all of the embedded videos in the document
+        var links = document.querySelectorAll("iframe[src^='https://www.youtube']");
+            
+        for(var x = 0; x < links.length; x += 1) {
+            if(links) {
+                // Get the embed URL
+                link = links[x].src;
+
+                if(link) {
+                    url = embedToWatch(link);
+
+                    if(url) {
+                        // open the URL in a new tab/window
+                        window.open(url);
+                    }
+                }
             }
         }
     }
